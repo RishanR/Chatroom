a255648aabb737cdd3cd4c99822b59265619bc42
RishanRatna <rishanratna@hotmail.com>
2020-09-20 11:47:24 -0400

    setting up server connection to front-end

--- client/src/components/Join/join.js
+++ client/src/components/Join/join.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../css/join.css';
+import './join.css';
 
 const backgroundImage = Math.floor((Math.random() * 8) + 1);
 
@@ -11,16 +11,15 @@ const Join = () => {
     return(
         <div className={`joinOuter joinOuterContainer${backgroundImage}`}>
             <div className="joinTint">
-                <form className="joinInnerContainer" onSubmit={(event) => {
-                    event.preventDefault();
-                    console.log(`Name: ${name}\nRoom: ${room}`);
-                }}>
-                    <h1>J o i n&nbsp;&nbsp;&nbsp;R o o m</h1>
+                <div className="joinInnerContainer">
+                    <h1 className="joinHeading">J o i n&nbsp;&nbsp;&nbsp;R o o m</h1>
                     <input id="inName" className="inputName" placeholder="&#xf2b9;&nbsp;&nbsp;Name" onChange={event => setName(event.target.value)}/>
                     <input id="inRoom" className="inputRoom" placeholder="&#xf015;&nbsp;&nbsp;Room" onChange={event => setRoom(event.target.value)}/>
                     <br />
-                    <button className="joinButton" type="submit">JOIN</button>
-                </form>
+                    <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={ `/chat?name=${name}&room=${room}`}>
+                        <button className="joinButton" type="submit">JOIN</button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
